4f2b8cf93974dd0b9f7ad0ffc82ef449ff14da8a
Juan G <juangupta@gmail.com>
2017-06-09 08:07:05 -0500

    Commit 20170609

--- CheckTicketJG/CheckTicketJG/CheckTicketJG/App.xaml.cs
+++ CheckTicketJG/CheckTicketJG/CheckTicketJG/App.xaml.cs
@@ -10,7 +10,7 @@ namespace CheckTicketJG
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new LoginPage());
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
